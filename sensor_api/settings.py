58f21ba68417d626908cb105a38946581a3042c3
Andreas Madsack <andreas@madflex.de>
2024-03-03 13:34:35 +0100

    add fly.io domain to ALLOWED_HOSTS

--- sensor_api/settings.py
+++ sensor_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,9 @@ SECRET_KEY = "django-insecure-g&bap52-c4m!kbd_owr*s#b)s30xl2aaxs5-%#%%m*zj^ops#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
 
 
 # Application definition
