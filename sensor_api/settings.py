390022ab5995c14aeacf4021f498ccd0c66210af
Andreas Madsack <andreas@madflex.de>
2024-03-23 14:12:17 +0100

    add email settings (using mailgun)

--- sensor_api/settings.py
+++ sensor_api/settings.py
@@ -146,3 +146,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if os.environ.get('EMAIL_HOST_PASSWORD'):
+    EMAIL_HOST = 'smtp.eu.mailgun.org'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = True
