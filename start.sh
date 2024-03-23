b96a19174f2eaff857c266b756a71a2a59cab397
Andreas Madsack <andreas@madflex.de>
2024-03-23 14:33:57 +0100

    use the same --no-input even if both ways are allowed

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 
 gunicorn --bind :8000 --workers 2 sensor_api.asgi:application -k uvicorn.workers.UvicornWorker
