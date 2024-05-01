f5f56fa7c0e063012b8e2dbf313d99daab76a8fd
Andreas Madsack <andreas@madflex.de>
2024-05-01 19:12:20 +0200

    add sentry

--- sensor_api/settings.py
+++ sensor_api/settings.py
@@ -16,6 +16,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.environ.get('SENTRY_DSN'):
+    import sentry_sdk
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        enable_tracing=True,
+    )
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
